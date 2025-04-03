61cf952bddb5e08fc363746418b32f82457ca4a9
경아 <sun4354@naver.com>
2025-04-03 15:18:58 +0900

    Add files via upload

--- js/datail.js
+++ js/datail.js
@@ -15,3 +15,46 @@ $('.modal_wrap, .pop .cancle_btn, .pop .close_btn').click(function () {
 $('.pop').click(function (e) {
 	e.stopPropagation();
 });
+
+/* 배너 슬라이드 */
+$('.swiper_wrap').each(function (t) {
+	$(this).find('.swiper, .swiper-pagination').addClass('type' + t);
+
+	const swiper = new Swiper('.swiper.type' + t, {
+		slidesPerView: 'auto',
+		effect: 'slide',
+		loop: false,
+		spaceBetween: 10,
+
+		on: {
+			slideChange: function () {
+				let totalSlides = this.slides.length;
+				let currentIndex = this.activeIndex;
+
+				if (currentIndex === totalSlides - 2) {
+					$('.swiper_wrap').eq(t).find('.slide_btn').css('display', 'none');
+				} else {
+					$('.swiper_wrap').eq(t).find('.slide_btn').css('display', 'block');
+
+				}
+			}
+		}
+	});
+});
+
+/* 슬라이드 높이 동일하게 */
+window.addEventListener("load", function () {
+	let slides = document.querySelectorAll(".swiper-slide");
+	let maxHeight = 0;
+
+	slides.forEach(slide => {
+		let height = slide.offsetHeight;
+		if (height > maxHeight) {
+			maxHeight = height;
+		}
+	});
+
+	slides.forEach(slide => {
+		slide.style.height = maxHeight + "px";
+	});
+});
