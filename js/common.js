7893a588f8dbe16a077e92199ba0b3750854bd45
경아 <sun4354@naver.com>
2025-04-21 18:41:02 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -8,3 +8,35 @@ $(document).ready(function () {
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
 	});
 });
+
+
+/* pop_up */
+const todayKey = 'hidePopupToday';
+const todayDate = new Date().toISOString().slice(0, 10);
+
+if (localStorage.getItem(todayKey) === todayDate) {
+	$('.pop_up').hide();
+}
+
+$('.pop_up, .pop_up .close_btn').click(function () {
+
+	if ($('#checkbox').is(':checked')) {
+		localStorage.setItem(todayKey, todayDate);
+	}
+	$('.pop_up').hide();
+});
+
+$('.pop_up, .pop_up .pop_content').click(function (e) {
+	e.stopPropagation();
+});
+
+$('.pop_content').on('click', '.img', function () {
+	if ($(this).hasClass('active')) {
+		window.location.href = 'https://www.naver.com';
+		return;
+	}
+	const newSrc = $(this).attr('src').replace('popup-01.png', 'popup-01-check.png');
+	$(this).attr('src', newSrc);
+	$(this).attr('alt', '광고성 메시지 수신 동의하기 체크 완료 및 알림 ON 참여하기 이동');
+	$(this).addClass('active');
+});
