85ec2124b018271183c39773630a1ddefa19d2a4
경아 <sun4354@naver.com>
2025-04-22 11:26:17 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -20,23 +20,25 @@ if (localStorage.getItem(todayKey) === todayDate) {
 
 $('.pop_up, .pop_up .close_btn').click(function () {
 
-	if ($('#checkbox').is(':checked')) {
+	if ($('#checkbox2').is(':checked')) {
 		localStorage.setItem(todayKey, todayDate);
 	}
 	$('.pop_up').hide();
 });
 
-$('.pop_up, .pop_up .pop_content').click(function (e) {
+
+$('.pop_wrap').click(function (e) {
 	e.stopPropagation();
 });
 
-$('.pop_content').on('click', '.img', function () {
-	if ($(this).hasClass('active')) {
+$('.pop_up .btn_wrap button').click(function () {
+	if ($('#checkbox').is(':checked')) {
 		window.location.href = 'https://www.naver.com';
-		return;
+	} else {
+		Swal.fire({
+			title: "광고성 메시지 수신 <br> 동의를 해주세요.",
+			background: "#fff",
+			confirmButtonText: "확인",
+		});
 	}
-	const newSrc = $(this).attr('src').replace('popup-01.png', 'popup-01-check.png');
-	$(this).attr('src', newSrc);
-	$(this).attr('alt', '광고성 메시지 수신 동의하기 체크 완료 및 알림 ON 참여하기 이동');
-	$(this).addClass('active');
 });
